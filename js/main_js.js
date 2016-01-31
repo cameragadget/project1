337cdbdd048c4a300806046bc536766f00ff06c5
jonah sobol <jonah.sobol@gmail.com>
2016-01-30 17:23:31 -0800

    adds ability to draw board from array of pieces

--- js/main_js.js
+++ js/main_js.js
@@ -1,9 +1,19 @@
 console.log("main_js loaded")
 
+var canvas = document.getElementById('mainCanvas');
+var ctx = canvas.getContext('2d');
 
+var gameBoard = {
+  x: 525,
+  y: 700,
+  columns: 10,
+  rows: 10,
+  tilewidth: 50,
+  tileheight: 50,
+  tiles: [],
+  rowHeight: 42,
+}
 
-  var canvas = document.getElementById('mainCanvas');
-    var ctx = canvas.getContext('2d');
 
 
 
@@ -24,70 +34,40 @@ var tileball = ballArray[Math.floor(Math.random()*ballArray.length)];
 
 var randomBall = ballArray[Math.floor(Math.random()*ballArray.length)];
 
+var testArray = [
+[ball1, ball2, ball2, ball5, ball5, ball5, ball2, ball3, ball3, ball1],
+[ball2, ball2, ball4, ball5, ball5, ball5, ball2, ball3, ball3, ball1],
+[ball3, ball3, ball3, ball5, ball5, ball5, ball2, ball3, ball3, ball1],
+[ball4, ball4, ball4, ball4, ball5, ball5, ball2, ball3, ball3, ball1],
+[ball5, ball5, ball5, ball5, ball5, ball4, ball2, ball3, ball3, ball1]
+]
 
+
+
+// this creates a game board, but does not enter pieces or coordinates
+// into the scoring array. I will need to revisit this when I complete
+// the scoring array and functionality to integrate.  this is
+// simply proof of concept.
 function draw() {
 var x = canvas.width;
 var y = canvas.height;
 var height = 50;
 var width = 50;
   for (var i = 0; i < 5; i++) {
-    var yPos = (height*i);
+    if (i < 1) {
+      var yPos = (height*i);
+    } else {
+      yPos = (gameBoard.rowHeight*i);
+    }
       for(var j=0; j < 10; j++){
         if (i%2 === 0) {
-        ctx.drawImage(ballArray[Math.floor(Math.random()*ballArray.length)],(width)*j, yPos, width, height);
+          ctx.drawImage(testArray[i][j],(width)*j, yPos, width, height);
+        //ctx.drawImage(ballArray[Math.floor(Math.random()*ballArray.length)],(width)*j, yPos, width, height);
       } else {
-        ctx.drawImage(ballArray[Math.floor(Math.random()*ballArray.length)],((width)*j)+(width/2), yPos, width, height);
+        ctx.drawImage(testArray[i][j],((width)*j)+(width/2), yPos, width, height);
       }
 }
 }
 }
 
 
-// function createLevel() {
-//   var x = 0;
-//   var y = 0;
-//   var height = 50;
-//   var width = 50;
-//           // for (var j=0; j<6; j++) {
-//             for (var i=0; i<10; i++) {
-//               ctx.drawImage(ballArray[Math.floor(Math.random()*ballArray.length)], x, y, height, width);
-//               x += 50;
-//               // y += 50;
-
-//             }
-//             // }
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-// var x = 0;
-// var y = 0;
-// var dx = 0;
-// var dy = 0;
-// var WIDTH;
-// var HEIGHT;
-// var ctx = document.getElementById("mainCanvas").getContext("2d");
-// ctx.drawImage(tileball, 64, 64);
-// ctx.fill();
-// function init() {
-//     var ctx = document.getElementById("mainCanvas").getContext("2d");
-//     return setInterval(draw, 10);
-// }
-// function draw() {
-//     ctx.drawImage(tileball, x, y, 64, 64);
-//     ctx.fill();
-//     x += dx;
-//     y += dy;
-// }
-// }
-// }
-// init();
