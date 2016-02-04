c57ccf0c4c5260d9194c0aa7dc0b90252d69c779
jonah sobol <jonah.sobol@gmail.com>
2016-02-03 18:12:29 -0800

    phil's small tweaks

--- js/main_js.js
+++ js/main_js.js
@@ -18,7 +18,7 @@ var gameBoard = {
   tileheight: 50,
   tiles: [],
   rowHeight: 42,
-  radius: 20,
+  radius: 20
 };
 
 var Tile = function(row, col, type) {
@@ -69,25 +69,25 @@ var makeGameBoard = function() {
 //this function then draws those tiles out.
 
 function drawTiles() {
-var x = canvas.width;
-var y = canvas.height;
-var height = 50;
-var width = 50;
+  var x = canvas.width;
+  var y = canvas.height;
+  var height = 50;
+  var width = 50;
   for (var i = 0; i < gameBoard.rows; i++) {
     if (i < 1) {
       var yPos = (height*i);
     } else {
       yPos = (gameBoard.rowHeight*i);
     }
-      for(var j=0; j < 10; j++){
-        if (gameBoard.tiles[i][j].type !== -1) {
-          if (i%2 === 0) {
-          ctx.drawImage(ballArray[gameBoard.tiles[i][j].type],(width)*j, yPos, width, height);
-          } else {
-          ctx.drawImage(ballArray[gameBoard.tiles[i][j].type],((width)*j)+(width/2), yPos, width, height);
-          }
+    for(var j=0; j < 10; j++){
+      if (gameBoard.tiles[i][j].type !== -1) {
+        if (i%2 === 0) {
+        ctx.drawImage(ballArray[gameBoard.tiles[i][j].type],(width)*j, yPos, width, height);
+        } else {
+        ctx.drawImage(ballArray[gameBoard.tiles[i][j].type],((width)*j)+(width/2), yPos, width, height);
         }
       }
+    }
   }
 }
 
@@ -103,15 +103,16 @@ var player = {
   y: 0,
   angle: 0,
   tiletype: -1,
-  emoji: {x: 0,
-          y: 0,
-          row: 0,
-          col: 0,
-          angle: 0,
-          speed: 0,
-          radius: 20,
-          tiletype: -1,
-         },
+  emoji: {
+    x: 0,
+    y: 0,
+    row: 0,
+    col: 0,
+    angle: 0,
+    speed: 0,
+    radius: 20,
+    tiletype: -1,
+  }
 };
 
   // assign player a ball type randomly
@@ -144,8 +145,8 @@ var drawPlayerBall = function() {
 var evenRowTouching = [[-1,-1], [-1,0], [0,-1], [0,1], [1,-1], [1,0]];
 var oddRowTouching = [[-1,0], [-1,1], [0,-1], [0,1], [1,0], [1,1]];
 
-  //somehow we need to be able to identify the array coodinates
-  //of the playerBall eve
+// somehow we need to be able to identify the array coodinates
+// of the playerBall eve
 // even though we have incorporated it already
 
 // var toCheck= [];
@@ -187,25 +188,25 @@ var oddRowTouching = [[-1,0], [-1,1], [0,-1], [0,1], [1,0], [1,1]];
 var cluster = [];
 
 function findMatch(trow, tcol) {
-
   var touching = trow % 2 === 0 ? evenRowTouching : oddRowTouching;
-    for (k = 0; k < touching.length; k++) {
-      var calcrow = trow + touching[k][0];
-      var calccol = tcol + touching[k][1];
-      var funtile = gameBoard.tiles[calcrow][calccol];
-        if ((calcrow < 13) && (calcrow > -1) && (calccol < 10) && (calccol > -1)){
-          if ((funtile.type === player.emoji.tiletype) && (funtile.checked === false)) {
-            funtile.matched = true;
-            cluster.push(funtile);
-          }
+  for (k = 0; k < touching.length; k++) {
+    var calcrow = trow + touching[k][0];
+    var calccol = tcol + touching[k][1];
+    var funtile = gameBoard.tiles[calcrow][calccol];
+      if ((calcrow < 13) && (calcrow > -1) && (calccol < 10) && (calccol > -1)){
+        if ((funtile.type === player.emoji.tiletype) && (funtile.checked === false)) {
+          funtile.matched = true;
+          cluster.push(funtile);
         }
-    }
-    recluster(cluster);
+      }
+  }
+  recluster(cluster);
 };
 
 // now we need to run findMatch on the contents of cluster
 
 var clusterSize = 0
+
 function recluster(cluster) {
   console.log(cluster);
   for (var i = 0; i < cluster.length; i++) {
@@ -231,8 +232,8 @@ function eliminateCluster() {
   foundMatch();
   if (clusterFound === true) {
     for (var i = 0; i < cluster.length; i++){
-    gameBoard.tiles[cluster[i].row][cluster[i].col].type = -1;
-    gameBoard.tiles[cluster[i].row][cluster[i].col].aplha = 0;
+      gameBoard.tiles[cluster[i].row][cluster[i].col].type = -1;
+      gameBoard.tiles[cluster[i].row][cluster[i].col].aplha = 0;
     }
   }
   clear();
@@ -242,9 +243,9 @@ function eliminateCluster() {
 }
 
 function clear() {
-var canvas = document.getElementById('mainCanvas');
-var ctx = canvas.getContext('2d');
-ctx.clearRect(0, 0, 525, 700);
+  // var canvas = document.getElementById('mainCanvas');
+  // var ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, 525, 700);
 }
 
 function reUp() {
@@ -262,27 +263,27 @@ function setUp() {
 };
 
 function radToDeg(angle) {
-    return angle * (180 / Math.PI);
+  return angle * (180 / Math.PI);
 }
 
 
 function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-  }
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
 }
 
 canvas.addEventListener('mousemove', function(evt) {
   var mousePos = getMousePos(canvas, evt);
   console.log('Mouse position: ' + mousePos.x + ',' + mousePos.y);
   var mouseangle = radToDeg(Math.atan2((player.y+gameBoard.tileheight/2) - mousePos.y, mousePos.x - (player.x+gameBoard.tilewidth/2)));
-    if (mouseangle < 0) {
-      mouseangle = 180 + (180 + mouseangle);
-    }
-      player.angle = mouseangle;
-      renderMouseAngle();
+  if (mouseangle < 0) {
+    mouseangle = 180 + (180 + mouseangle);
+  }
+  player.angle = mouseangle;
+  renderMouseAngle();
 }, false);
 
 canvas.addEventListener('click', function() {
@@ -294,44 +295,43 @@ canvas.addEventListener('click', function() {
 
 
 
-
 function movePlayer() {
-    var drawPlayerEmoji = function(px, py) {
-      ctx.drawImage(ballArray[player.emoji.tiletype], px , py,
-      gameBoard.tilewidth, gameBoard.tileheight);
+  var drawPlayerEmoji = function(px, py) {
+    ctx.drawImage(ballArray[player.emoji.tiletype], px , py,
+    gameBoard.tilewidth, gameBoard.tileheight);
+  }
+  player.emoji.x = player.x;
+  player.emoji.y = player.y;
+  player.emoji.angle = player.angle;
+  player.emoji.tiletype = player.tiletype;
+  player.emoji.speed = 1.2;
+  var dx = player.emoji.speed * Math.cos(degToRad(player.emoji.angle));
+  var dy = player.emoji.speed * -1*Math.sin(degToRad(player.emoji.angle));
+  moveInterval = setInterval(function() {
+    if (!moving){
+      clearInterval(moveInterval);
     }
-        player.emoji.x = player.x;
-        player.emoji.y = player.y;
-        player.emoji.angle = player.angle;
-        player.emoji.tiletype = player.tiletype;
-        player.emoji.speed = .5;
-        var dx = player.emoji.speed * Math.cos(degToRad(player.emoji.angle));
-        var dy = player.emoji.speed * -1*Math.sin(degToRad(player.emoji.angle));
-        var moveInterval = setInterval(function(){
-          if (!moving){
-            clearInterval(moveInterval);
-          }
-            drawPlayerEmoji(player.emoji.x += dx, player.emoji.y += dy);
-            detectCollision();
-        }, 5);
-        moveInterval;
+    drawPlayerEmoji(player.emoji.x += dx, player.emoji.y += dy);
+    detectCollision();
+  }, 5);
+  // moveInterval;
 };
 
 
 function degToRad(angle) {
-    return angle * (Math.PI / 180);
+  return angle * (Math.PI / 180);
 }
 
 function renderMouseAngle() {
-    var centerx = player.x + gameBoard.tilewidth/2;
-    var centery = player.y + gameBoard.tileheight/2;
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = "#000000";
-    ctx.beginPath();
-    ctx.moveTo(centerx, centery);
-    ctx.lineTo(centerx + 1.5*gameBoard.tilewidth * Math.cos(degToRad(player.angle)),
-               centery - 1.5*gameBoard.tileheight * Math.sin(degToRad(player.angle)));
-    ctx.stroke();
+  var centerx = player.x + gameBoard.tilewidth/2;
+  var centery = player.y + gameBoard.tileheight/2;
+  ctx.lineWidth = 4;
+  ctx.strokeStyle = "#000000";
+  ctx.beginPath();
+  ctx.moveTo(centerx, centery);
+  ctx.lineTo(centerx + 1.5*gameBoard.tilewidth * Math.cos(degToRad(player.angle)),
+             centery - 1.5*gameBoard.tileheight * Math.sin(degToRad(player.angle)));
+  ctx.stroke();
 }
 
 // if length >= 3, the tile type is changed to -1 and they vanish
@@ -342,59 +342,61 @@ function renderMouseAngle() {
 
 
 setInterval(function() {
-    clear();
-    drawGame();
-    drawTiles();
-    renderMouseAngle();
-    drawPlayerBall();
-    gameOver();
-    findWin();
+  clear();
+  drawGame();
+  drawTiles();
+  renderMouseAngle();
+  drawPlayerBall();
+  gameOver();
+  findWin();
 }, 40);
 
 
 var distance;
 var collisionTile;
 var detectCollision = function() {
-    for (var i = 0; i < gameBoard.rows; i++){
-      for (var j = 0; j < gameBoard.columns; j++){
-            if (i % 2 === 0) {
-            gameBoard.tiles[i][j].x = j * gameBoard.tilewidth + gameBoard.tilewidth/2;
-            gameBoard.tiles[i][j].y = i * gameBoard.rowHeight + gameBoard.rowHeight/2;
-            } else {
-            gameBoard.tiles[i][j].x = j * gameBoard.tilewidth + gameBoard.tilewidth;
-            gameBoard.tiles[i][j].y = i * gameBoard.rowHeight + gameBoard.rowHeight/2;
-            }
-            var dx = gameBoard.tiles[i][j].x - player.emoji.x;
-            var dy = gameBoard.tiles[i][j].y - player.emoji.y;
-            distance = Math.sqrt(dx * dx + dy * dy);
-              if (gameBoard.tiles[i][j].type >= 0) {
-                if (distance < gameBoard.radius + player.emoji.radius) {
-                  console.log("collision detected!");
-                  console.log(player.emoji.x, player.emoji.y);
-                  console.log(gameBoard.tiles[i][j]);
-                  moving = false;
-                  closestTileCoordinate(player.emoji.x, player.emoji.y)
-                }
-              }
+  for (var i = 0; i < gameBoard.rows; i++){
+    for (var j = 0; j < gameBoard.columns; j++){
+      if (i % 2 === 0) {
+        gameBoard.tiles[i][j].x = j * gameBoard.tilewidth + gameBoard.tilewidth/2;
+        gameBoard.tiles[i][j].y = i * gameBoard.rowHeight + gameBoard.rowHeight/2;
+      } else {
+        gameBoard.tiles[i][j].x = j * gameBoard.tilewidth + gameBoard.tilewidth;
+        gameBoard.tiles[i][j].y = i * gameBoard.rowHeight + gameBoard.rowHeight/2;
+      }
+      var dx = gameBoard.tiles[i][j].x - player.emoji.x;
+      var dy = gameBoard.tiles[i][j].y - player.emoji.y;
+      distance = Math.sqrt(dx * dx + dy * dy);
+      if (gameBoard.tiles[i][j].type >= 0) {
+        if (distance < gameBoard.radius + player.emoji.radius) {
+          clearInterval(moveInterval);
+          console.log("collision detected!");
+          console.log(player.emoji.x, player.emoji.y);
+          console.log(gameBoard.tiles[i][j]);
+          moving = false;
+          closestTileCoordinate(player.emoji.x, player.emoji.y);
+          return;
+        }
       }
     }
+  }
 };
 
 var closestTileCoordinate = function(x, y) {
   var xToCol;
   var yToRow = Math.round(((y+25)/42)-1);
-    if (yToRow % 2 === 0) {
-      xToCol = Math.round(((x+25)/50)-1);
-    }   else {
-      xToCol = Math.round(((x+50)/50)-1);
-    }
-      console.log(yToRow, xToCol);
-      snapTile(yToRow, xToCol);
-      // gameBoard.tiles[yToRow][xToCol].checked = true;
-      // toCheck.push(gameBoard.tiles[yToRow][xToCol]);
-      // findMatch();
-      findMatch(yToRow, xToCol);
-      eliminateCluster();
+  if (yToRow % 2 === 0) {
+    xToCol = Math.round(((x+25)/50)-1);
+  }   else {
+    xToCol = Math.round(((x+50)/50)-1);
+  }
+  console.log(yToRow, xToCol);
+  snapTile(yToRow, xToCol);
+  // gameBoard.tiles[yToRow][xToCol].checked = true;
+  // toCheck.push(gameBoard.tiles[yToRow][xToCol]);
+  // findMatch();
+  findMatch(yToRow, xToCol);
+  eliminateCluster();
 };
 
 var snapTile = function(row, col) {
@@ -426,8 +428,5 @@ var gameOver = function() {
     }
   }
 }
-
-
-
 
 
